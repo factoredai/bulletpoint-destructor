b3bc0cfa08d0a8942a54a2f62b4c51919be51f45
JuanD5 <juancastro97125@gmail.com>
2024-05-06 11:29:26 -0500

    Fix the method to access the openai api key

--- src/utils/configs.py
+++ src/utils/configs.py
@@ -1,8 +1,7 @@
 import os
 
-import openai
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(dotenv_path=find_dotenv(filename=".env"))
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+OPENAI_API_KEY = os.getenv(key="OPENAI_API_KEY")
